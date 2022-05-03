0eeb5ce047e3e0eef231fd070f67a0cffd369f11
Vera Kutiavina <vera.kutiavina@flatstack.com>
2022-05-04 01:52:02 +0300

    Create Link model

--- linkshortener/models.py
+++ linkshortener/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+'''
+    long_link - the original link
+    short_link - new link
+    followed_count - count the link has been opened
+'''
+
+
+class Link(models.Model):
+    long_link = models.CharField(max_length=50)
+    short_link = models.CharField(max_length=50, unique=True)
+    followed_count = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.long_link + 'to' + self.short_link
