b3a5c0c762c15a48a69b3cd09c705bca3daee267
Vera Kutiavina <vera.kutiavina@flatstack.com>
2022-05-17 00:12:08 +0300

    Create main page

---
+++ linkshortener/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from .models import Link
+
+
+# Main form for create short link with validation
+
+class LinkShorterForm(forms.Form):
+    long_link = forms.URLField(required=True, max_length=50, widget=forms.URLInput(
+        attrs={"class": "input", "placeholder": "Write your URL here"}))
+
+    class Meta:
+        model = Link
+        fields = 'long_link,'
+
--- linkshortener/views.py
+++ linkshortener/views.py
@@ -1,5 +1,34 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Link
+from .forms import LinkShorterForm
 
 
 def index(request):
     return HttpResponse("Hello, world from links!")
+
+
+def create(request):
+    template = loader.get_template('links/home.html')
+    context = {'form': LinkShorterForm()}
+
+    if request.method == 'GET':
+        return HttpResponse(template.render(context, request))
+
+    elif request.method == 'POST':
+
+        used_form = LinkShorterForm(request.POST)
+
+        if used_form.is_valid():
+            user_link = request.POST['long_link']
+
+            link = Link(long_link=user_link)
+            link.save()
+
+            context['new_link'] = request.build_absolute_uri('/') + link.short_link
+            context['long_link'] = link.long_link
+
+            return HttpResponse(template.render(context, request))
+
+        return render(request, template, context)
