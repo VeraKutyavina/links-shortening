c3df90499b1eaac26320a7c46bc2e93d944b8301
Vera Kutiavina <vera.kutiavina@flatstack.com>
2022-05-17 23:19:05 +0300

    Encode url with Hash and Hash Salt

--- linkshortener/utils.py
+++ linkshortener/utils.py
@@ -1,29 +1,30 @@
-from random import choice
+import os
+import hashlib
 
-from string import ascii_letters, digits
-
-# Max url size
-URL_SIZE = 10
-AVAIABLE_CHARS = ascii_letters + digits
+LENGTH = 10
 
 
-# Creates a random string with url size length
+# Creates hash with salt for user url
 
-def create_random_string():
-    return "".join(
-        [choice(AVAIABLE_CHARS) for _ in range(URL_SIZE)]
-    )
+def generate_link_hash(long_link):
+    salt = os.urandom(32)
+    plaintext = long_link.encode()
+
+    # params: (hash algorithm, byte string of url, salt, e number of times the hash algorithm is called, length)
+    a = hashlib.pbkdf2_hmac('sha256', plaintext, salt, 10000, LENGTH).hex()
+
+    return a
 
 
 # Generate short link for url
 
 def generate_short_url(model_instance):
-    random_string = create_random_string()
+    link_hash = generate_link_hash(model_instance.long_link)
 
     model_class = model_instance.__class__
 
     # Call method again if string have already exist
-    if model_class.objects.filter(short_link=random_string).exists():
+    if model_class.objects.filter(short_link=link_hash).exists():
         return generate_short_url(model_instance)
 
-    return random_string
+    return link_hash
